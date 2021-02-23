0402feb19552e749a7f6854c38b5e550c8ae01d3
Vinícius Montanheiro <vmontanheiro@whisky.local>
2021-02-23 11:37:13 -0300

    fixing filenames

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const AWS = require('aws-sdk')
-const mammoth = require('mammoth')
-const pdf = require('pdf-parse')
+const AWS = require('aws-sdk');
+const mammoth = require('mammoth');
+const pdf = require('pdf-parse');
 
 const API_VERSION = `2013-01-01`
 
@@ -10,30 +10,34 @@ const getFileExtension = (filename) => {
   return extension || "";
 };
 
-const removeInvalidCharacters = (str) => str.replace(/[^0-9a-zA-Z/._-]/g, '')
+const removeInvalidFileContent = (str) => str.replace(/[^\u0009\u000a\u000d\u0020-\uD7FF\uE000-\uFFFD]/g, '');
 
-const decodeURISpaces = (str) => decodeURI(str).replace(/\+/g, ' ')
+const decodeURISpaces = (str) => decodeURI(str).replace(/\+/g, ' ');
 
-const isPDF = (filename) => getFileExtension(filename).toLowerCase().match(/\.pdf/g)
+const isPDF = (filename) => getFileExtension(filename).toLowerCase().match(/\.pdf/g);
 
-const isDOC = (filename) => getFileExtension(filename).toLowerCase().match(/\.docx/g)
+const isDOCX = (filename) => getFileExtension(filename).toLowerCase().match(/\.docx/g);
+
+const isDOC = (filename) => getFileExtension(filename).toLowerCase().match(/\.doc/g);
+
+const isDeleteEvent = (event) => !!event.Records[0].eventName.toLowerCase().match(/delete/);
 
 const getPDFText = async (buffer) => {
   try {
-    const text = (await pdf(buffer)).text
+    const text = (await pdf(buffer)).text;
     return text;
   } catch (error) {
-    console.log(`getPDFText error`, error)
+    console.log(`getPDFText error`, error);
     return "";
   }
 }
 
 const getDOCXText = async (buffer) => {
   try {
-    const text = (await mammoth.extractRawText({ buffer })).value
-    return text
+    const text = (await mammoth.extractRawText({ buffer })).value;
+    return text;
   } catch (error) {
-    console.log(`getDOCXText error`, error)
+    console.log(`getDOCXText error`, error);
     return "";
   }
 }
@@ -60,7 +64,7 @@ const getJBatch = async ({ bucketname, filename, buffer }) => {
       },
     ];
   }
-  if (isDOC(filename)) {
+  if (isDOCX(filename)) {
     const content = await getDOCXText(buffer);
     return [
       {
@@ -73,6 +77,20 @@ const getJBatch = async ({ bucketname, filename, buffer }) => {
       },
     ];
   }
+  if (isDOC(filename)) {
+    const content = removeInvalidFileContent(buffer.toString('utf-8'));
+    return [
+      {
+        type,
+        id,
+        fields: {
+          content,
+          ...fields
+        },
+      },
+    ];
+  }
+  
   const content = buffer.toString('utf-8');
   return [
     {
@@ -88,26 +106,26 @@ const getJBatch = async ({ bucketname, filename, buffer }) => {
 
 const addToCS = async ({ bucketname, filename, buffer, region, endpoint }) => {
   try {
-    console.log(`Starting CS upload to endpoint: `, endpoint)
+    console.log(`Starting CS upload to endpoint: `, endpoint);
 
     const csd = new AWS.CloudSearchDomain({
       endpoint,
       region,
       apiVersion: API_VERSION,
-    })
+    });
 
     const jbatch = await getJBatch({filename, bucketname, buffer});
 
     const params = {
       contentType: 'application/json',
       documents: JSON.stringify(jbatch),
-    }
+    };
 
-    const result = await csd.uploadDocuments(params).promise()
-    console.log('CS upload successfully!')
-    console.log(result)
+    const result = await csd.uploadDocuments(params).promise();
+    console.log('CS upload successfully!');
+    console.log(result);
   } catch (error) {
-    console.log('CS upload file error >>> ', error)
+    console.log('CS upload file error >>> ', error);
   }
 }
 
@@ -118,61 +136,61 @@ const deleteFromCSIndex = async ({
   endpoint,
 }) => {
   try {
-    console.log(`Starting CS file delete from endpoint: `, endpoint)
+    console.log(`Starting CS file delete from endpoint: `, endpoint);
 
     const csd = new AWS.CloudSearchDomain({
       endpoint,
       region,
       apiVersion: API_VERSION,
-    })
+    });
 
     const jbatch = [
       {
         type: 'delete',
         id: bucketname + ':' + filename,
       },
-    ]
+    ];
 
     const params = {
       contentType: 'application/json',
       documents: JSON.stringify(jbatch),
-    }
-    const result = await csd.uploadDocuments(params).promise()
-    console.log('CS file deleted successfully!')
-    console.log(result)
+    };
+    const result = await csd.uploadDocuments(params).promise();
+    console.log('CS file deleted successfully!');
+    console.log(result);
   } catch (error) {
-    console.log('CS delete file error >>> ', error)
+    console.log('CS delete file error >>> ', error);
   }
 }
 
 exports.handler = async (event) => {
   try {
-    const isDelete = !!event.Records[0].eventName.match(/Delete|delete/)
+    const isDelete = isDeleteEvent(event);
 
     console.log(
       `File indexer method: ${
         isDelete ? 'delete' : 'add'
       }`
-    )
+    );
 
-    const region = process.env.REGION
-    const configurationId = event.Records[0].s3.configurationId
-    const endpoint = `${configurationId}.${region}.cloudsearch.amazonaws.com`
-    const filename = decodeURISpaces(event.Records[0].s3.object.key)
-    const bucketname = event.Records[0].s3.bucket.name
+    const region = process.env.REGION;
+    const configurationId = event.Records[0].s3.configurationId;
+    const endpoint = `${configurationId}.${region}.cloudsearch.amazonaws.com`;
+    const filename = decodeURISpaces(event.Records[0].s3.object.key);
+    const bucketname = event.Records[0].s3.bucket.name;
 
     if (!isDelete) {
-      console.log(`Getting S3 file ${filename} from ${bucketname} ...`)
+      console.log(`Getting S3 file ${filename} from ${bucketname} ...`);
 
       const params = {
         Bucket: bucketname,
         Key: filename,
         RequestPayer: 'requester',
-      }
+      };
       const s3 = new AWS.S3({
         region,
-      })
-      const buffer = (await s3.getObject(params).promise()).Body
+      });
+      const buffer = (await s3.getObject(params).promise()).Body;
 
       await addToCS({
         bucketname,
@@ -180,16 +198,16 @@ exports.handler = async (event) => {
         buffer,
         region,
         endpoint,
-      })
+      });
     } else {
       await deleteFromCSIndex({
         bucketname,
         filename: removeInvalidCharacters(filename),
         region,
         endpoint,
-      })
+      });
     }
   } catch (error) {
-    console.log('Handler started with errors >>> ', error)
+    console.log('Handler started with errors >>> ', error);
   }
 }
